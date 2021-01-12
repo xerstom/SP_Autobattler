07347b41e90e107613c802edcbc16741a1213d98
Xerstom <xerstomdev@outlook.fr>
2021-01-12 12:11:17 +0100

    WIP

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Card from '../components/GamingArea/Card/Card'
+
+export default function Home() {
+    return ( <Card name = "Zeus"nickname = "The Supreme God"path = "Zeus.png"attack = "10" life = "100" > </Card>
+    );
+}

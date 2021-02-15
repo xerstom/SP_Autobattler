272952939186df0f283be428a2f57c25aae3f8e5
Xerstom <xerstomdev@outlook.fr>
2021-02-15 16:25:44 +0100

    feat:  basic grid + selected

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Card from '../components/GamingArea/Card/Card'
+import Grid from '../components/Grid/Grid'
 
 export default function Home() {
-    return ( <Card name = "Zeus"nickname = "The Supreme God"path = "Zeus.png"attack = "10" life = "100" > </Card>
-    );
+    // return ( <Card name = "Zeus"nickname = "The Supreme God"path = "Zeus.png"attack = "10" life = "100" > </Card>
+	return (<Grid columns={10} rows={10}></Grid>);
 }

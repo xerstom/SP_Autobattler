7a91b1b34ae486c3a88b12bde1081a6e5c99d836
Khaaz <khaaz.dev@gmail.com>
2021-04-24 16:33:36 +0000

    Deploying to gh-pages from  @ 0a87e4faad6e2b8ce2b9f70aa7d9b26611b7cd4b 🚀

---
+++ _next/static/vU0KaZXdbJh6iODuqCXnJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-350dc18475b3bf8e62ef.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5594dfa33fcee2352441.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
